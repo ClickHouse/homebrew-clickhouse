1b5eef6e8772e0a19b59a6eecbf8e10a43d35ea1
Denis Glazachev <d.glazachev@gmail.com>
2021-12-14 04:36:06 +0300

    Add process_type and root_dir

--- Formula/clickhouse@21.8-altinity-stable.rb
+++ Formula/clickhouse@21.8-altinity-stable.rb
@@ -109,6 +109,8 @@ class ClickhouseAT218AltinityStable < Formula
     ]
     keep_alive true
     run_type :immediate
+    process_type :standard
+    root_dir var
     working_dir var
     log_path var/"log/clickhouse-server/stdout.log"
     error_log_path var/"log/clickhouse-server/stderr.log"
